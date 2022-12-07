849858581f6f6b7f90472b7fe49eabef44e68795
Jiseeeh <johncarlo.camara1@gmail.com>
2022-12-07 23:37:58 +0800

    feat: add default view value

--- interfaces/Thought.ts
+++ interfaces/Thought.ts
@@ -9,7 +9,7 @@ enum Tags {
 export default interface Thought {
   id?: string;
   createdAt?: string;
-  views: number;
+  views: 0 | number;
   ownerName: "Anonymous" | string;
   tag: Tags;
   content: string;

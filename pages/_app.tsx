ad6b9eefd76c8069ccdb5feed270007dc28743a7
Jiseeeh <johncarlo.camara1@gmail.com>
2022-12-08 00:08:02 +0800

    feat: add theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Theme } from "react-daisyui";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Theme dataTheme="light">
+      <main className="min-h-screen">
+        <Component {...pageProps} />
+      </main>
+    </Theme>
+  );
 }

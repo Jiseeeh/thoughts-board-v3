e827455ec35f221bf6c421efb6e5cf71d39573ec
Jiseeeh <johncarlo.camara1@gmail.com>
2022-12-10 16:17:48 +0800

    feat: add global footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { useState } from "react";
-import { Theme, Navbar, Button, Swap, SwapProps } from "react-daisyui";
-import { IconSunHigh } from "@tabler/icons";
-import { IconMoon } from "@tabler/icons";
+import { Theme, Navbar, Button, Swap, SwapProps, Footer } from "react-daisyui";
+import {
+  IconSunHigh,
+  IconMoon,
+  IconBadges,
+  IconFlower,
+  IconBrandGithub,
+  IconBrandLinkedin,
+  IconBrandNextjs,
+  IconBrandTailwind,
+  IconBrandMongodb,
+  IconBrandPrisma,
+} from "@tabler/icons";
 
 const swapProps: SwapProps = {
   rotate: true,
@@ -16,7 +26,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <Theme dataTheme={`${isDarkMode ? "dark" : "light"}`}>
-      <main className="min-h-screen">
+      <main className="min-h-screen flex flex-col">
         {/* global navbar */}
         <Navbar className="flex justify-between">
           <section className="md:ml-auto">
@@ -35,6 +45,46 @@ export default function App({ Component, pageProps }: AppProps) {
         {/* main content */}
         <Component {...pageProps} />
         {/* global footer */}
+        <Footer className="p-5 mt-auto bg-neutral text-neutral-content">
+          {/* TODO: Add links */}
+          <section>
+            <IconBadges size={100} />
+            <section className="flex items-center">
+              <Button>
+                <IconBrandGithub />
+              </Button>
+              <Button>
+                <IconBrandLinkedin />
+              </Button>
+              <span className="font-bold">&#8211; Jiseeeh</span>
+            </section>
+          </section>
+          <section>
+            <Footer.Title>Tech used</Footer.Title>
+            <article className="flex flex-col">
+              <section className="flex">
+                <span>Next.js &nbsp;</span>
+                <IconBrandNextjs />
+              </section>
+              <section className="flex">
+                <span>Tailwindcss &nbsp;</span>
+                <IconBrandTailwind />
+              </section>
+              <section className="flex">
+                <span>MongoDB &nbsp;</span>
+                <IconBrandMongodb />
+              </section>
+              <section className="flex">
+                <span>Prisma &nbsp;</span>
+                <IconBrandPrisma />
+              </section>
+              <section className="flex">
+                <span>React-DaisyUI &nbsp;</span>
+                <IconFlower />
+              </section>
+            </article>
+          </section>
+        </Footer>
       </main>
     </Theme>
   );

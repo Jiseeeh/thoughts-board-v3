52a7fc688ab477de8285625c26d5258f47b9686f
Jiseeeh <johncarlo.camara1@gmail.com>
2022-12-15 21:59:05 +0800

    feat: add filter404

---
+++ components/Filter404.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Image from "next/image";
+
+const Filter404: React.FC = () => {
+  return (
+    <section className="relative h-96 md:h-screen">
+      <h1 className="pt-5 text-center font-bold text-2xl md:text-4xl">
+        There's always more to see.
+      </h1>
+      <Image
+        src="/filter404.svg"
+        alt="Illustration of a woman looking for something with a telescope while riding a bird."
+        fill
+      />
+    </section>
+  );
+};
+
+export default Filter404;

e406da4f48134cef4a42ff2586326a0f26035565
Jiseeeh <johncarlo.camara1@gmail.com>
2022-12-16 15:01:59 +0800

    feat: api route for updating thought views

---
+++ pages/api/thoughts/[thoughtId].ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { prisma } from "../../../lib/prisma";
+
+export default async function updateThought(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { thoughtId } = req.query;
+
+  try {
+    const updatedThought = await prisma.thought.update({
+      where: {
+        id: String(thoughtId),
+      },
+      data: {
+        views: {
+          increment: 1,
+        },
+      },
+    });
+
+    res.status(200).json({ success: true, updatedThought });
+  } catch (error) {
+    res.status(500).json({ success: false, error });
+  }
+}

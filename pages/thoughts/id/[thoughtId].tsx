6ddc60b2d86a855aa4f8d6e42d6205fa0538da0d
Jiseeeh <johncarlo.camara1@gmail.com>
2022-12-16 15:52:06 +0800

    feat: add dynamic route for thoughts

---
+++ pages/thoughts/id/[thoughtId].tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import useSWR from "swr";
+import { useRouter } from "next/router";
+import axios from "../../../lib/axios";
+
+const fetcher = async (url: string) => {
+  const response = await axios.get(url);
+  const data = response.data;
+
+  if (data.success) return data.thought;
+  return new Error(data.error);
+};
+
+const SingleThought: React.FC = (props) => {
+  const router = useRouter();
+  const { data, error, isLoading } = useSWR(
+    `/api/thoughts/get/${router.query.thoughtId}`,
+    fetcher
+  );
+
+  console.log(data);
+  return <section>HEY</section>;
+};
+
+export default SingleThought;

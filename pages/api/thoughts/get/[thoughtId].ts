38573af56400c89ceca787a16ee5d63588bab159
Jiseeeh <johncarlo.camara1@gmail.com>
2022-12-16 15:51:45 +0800

    feat: api route for getting a single thought

---
+++ pages/api/thoughts/get/[thoughtId].ts
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { prisma } from "../../../../lib/prisma";
+
+export default async function getSingleThought(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { thoughtId } = req.query;
+
+  try {
+    const thought = await prisma.thought.findFirstOrThrow({
+      where: {
+        id: String(thoughtId),
+      },
+    });
+
+    res.status(200).json({ success: true, thought });
+  } catch (error) {
+    res.status(500).json({ success: false, error });
+  }
+}

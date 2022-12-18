82680f83f02059be6da2e03a108d4ae68618f0db
Jiseeeh <johncarlo.camara1@gmail.com>
2022-12-18 14:00:02 +0800

    feat: fetch single thought

--- pages/thoughts/id/[thoughtId].tsx
+++ pages/thoughts/id/[thoughtId].tsx
@@ -1,34 +1,62 @@
 import React from "react";
 import useSWR from "swr";
 import Head from "next/head";
+import moment from "moment";
+import { IconClock } from "@tabler/icons";
 import { useRouter } from "next/router";
 
 import axios from "@lib/axios";
+import IThought from "@interfaces/IThought";
+import generateTag from "@helper/generateTag";
 
-const fetcher = async (url: string, body: string) => {
+const fetcher = async (url: string, thoughtId: string) => {
   const response = await axios.get(url, {
-    data: body,
+    params: {
+      thoughtId,
+    },
   });
+
   const data = response.data;
 
   if (data.success) return data.thought;
   return new Error(data.error);
 };
 
-const SingleThought: React.FC = (props) => {
+const SingleThought: React.FC = () => {
   const router = useRouter();
-  const { data, error, isLoading } = useSWR(
+  const {
+    data,
+    error,
+    isLoading,
+  }: { data: IThought; error: any; isLoading: boolean } = useSWR(
     ["/api/getThought", String(router.query.thoughtId)],
     ([url, body]) => fetcher(url, body)
   );
 
-  console.log(data);
   return (
     <>
       <Head>
         <title>Thought</title>
       </Head>
-      <section>HEY</section>
+      <article className="p-10 flex flex-col items-center">
+        <section className="mb-3 flex flex-col">
+          {/* tag */}
+          {data && <span className="self-center">{generateTag(data.tag)}</span>}
+          {/* name & created at */}
+          <section className="flex">
+            {data && <span>By {data.ownerName}&nbsp;</span>}
+            {data && (
+              <span className="flex">
+                <IconClock /> {moment(data.createdAt).fromNow()}
+              </span>
+            )}
+          </section>
+        </section>
+        {/* content */}
+        <section className="max-w-3xl leading-7">
+          <p>{data && data.content}</p>
+        </section>
+      </article>
     </>
   );
 };

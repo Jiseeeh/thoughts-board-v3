66836de6dc7971bd3fe745bccae1a8e868b6b18e
Jiseeeh <johncarlo.camara1@gmail.com>
2022-12-18 14:45:40 +0800

    feat: add title prop to make it more flexible

--- components/Thoughts404.tsx
+++ components/Thoughts404.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Image from "next/image";
 
-const Thoughts404: React.FC = () => {
+interface Thoughts404Props {
+  title: string;
+}
+
+const Thoughts404: React.FC<Thoughts404Props> = ({ title }) => {
   return (
     <section className="h-96 relative md:h-screen">
       <h1 className="pt-5 text-center font-bold text-2xl md:text-4xl">
-        Couldn&#39;t find thoughts.
+        {title}
       </h1>
       <Image
         className="object-cover"

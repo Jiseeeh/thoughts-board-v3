90f548be9838ebea80f597ea620682161c3ec330
Jiseeeh <johncarlo.camara1@gmail.com>
2022-12-19 18:35:22 +0800

    style(margin): fix overlap

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -7,7 +7,7 @@ interface LoadingProps {
 
 const Loading: React.FC<LoadingProps> = ({ size }) => {
   return (
-    <section className="mb-3 flex justify-center">
+    <section className="my-3 flex justify-center">
       <HashLoader color="#36d7b7" size={size} />
     </section>
   );
